9cb7d75d389c2e31260accbb16cc00372c9c3620
myicewater <1026795843@qq.com>
2016-04-12 17:01:29 +0800

    pic deal util

--- src/plasea/PlaseaUtil.java
+++ src/plasea/PlaseaUtil.java
@@ -1,4 +1,10 @@
 package plasea;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * 一些常用方法工具类
  * @author 朱素海
@@ -18,11 +24,56 @@ public class PlaseaUtil {
 		return false;
 	}
 	
+	/**
+	 * 判断两个日期是否是同一天
+	 * @param d1
+	 * @param d2
+	 */
+	public static boolean isSameDay(Calendar d1,Calendar d2){
+		if(d1.get(Calendar.YEAR) == d2.get(Calendar.YEAR)
+				&& d1.get(Calendar.MONTH) == d2.get(Calendar.MONTH)
+				&& d1.get(Calendar.DATE ) == d2.get(Calendar.DATE )){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * 判断两个日期是否是同一年
+	 * @param d1
+	 * @param d2
+	 */
+	public static boolean isSameYear(Calendar d1,Calendar d2){
+		if(d1.get(Calendar.YEAR) == d2.get(Calendar.YEAR)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
 	
 	
 	public static void main(String[] args) {
 
-		System.out.println(isNullOrNullString("a"));
+		Date d1 = new Date();
+		
+		try {
+			Date d2 = new SimpleDateFormat("yyyy-MM-dd").parse("2016-04-09");
+			Calendar c1 = Calendar.getInstance();
+			Calendar c2 = Calendar.getInstance();
+			c1.setTime(d1);
+			c2.setTime(d2);
+			System.out.println(isSameDay(c1,c2));
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
+	
+	
 
 }
